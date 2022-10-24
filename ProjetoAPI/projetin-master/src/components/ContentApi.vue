bc5b7c2e5913308144f959309a68075b046085c1
Eron Tiago <94570966+eronzin13@users.noreply.github.com>
2022-10-24 15:36:33 -0300

    Add files via upload

---
+++ ProjetoAPI/projetin-master/src/components/ContentApi.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card" style="margin: 5px; width: 18rem;">
+  <img src="../assets/download.png" style=" width: 275px;" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Filmes</h5>
+    <p class="card-text">Comedia</p>
+   
+  </div>
+</div>
+</template>
+<style scoped>
+.card{
+  box-shadow: 1px 1px 6px 1px #000;
+  background-color: rgb(177, 177, 177);
+}
+</style>
---
+++ ProjetoAPI/projetin-master/src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<script setup>
+import ContentApi from "../components/ContentApi.vue";
+</script>
+
+<template>
+  <div class="fundo">
+    <h3>Metaview</h3>
+    <div class="input-group">
+      <input
+        type="search"
+        style="max-width: 600px"
+        class="form-control rounded"
+        placeholder="Procure aqui seu filme"
+        aria-label="Search"
+        aria-describedby="search-addon"
+      />
+      <button type="button" class="btn btn-primary">Clique aqui</button>
+    </div>
+  </div>
+  <div class="container">
+    <ContentApi></ContentApi>
+    <ContentApi></ContentApi>
+    <ContentApi></ContentApi>
+    <ContentApi></ContentApi>
+    <ContentApi></ContentApi>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+}
+.input-group {
+  margin-bottom: 65px;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+}
+h3 {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.fundo {
+  
+  background-color: rgb(110, 110, 110);
+  padding: 3%;
+ margin-right: 22%;
+ margin-left: 22%;
+ border-radius: 24px;
+ box-shadow: 0 0 24px #000;
+ }
+</style>
